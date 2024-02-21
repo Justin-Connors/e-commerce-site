55fbab1a105c26da752f0038f22b7298720585d9
justin connors <justinconnors@hotmail.ca>
2024-02-20 21:18:20 -0400

    started work on footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,13 +1,25 @@
 import React from "react";
+import { makeStyles } from "@mui/material/styles";
+import { Typography, Link } from "@mui/material";
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    padding: theme.spacing(6),
+  },
+}));
 
 const Footer = () => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h1>Footer</h1>
-      <footer>
-        <p>© 2021</p>
-      </footer>
-    </div>
+    <footer className={classes.footer}>
+      <Typography variant="body2" color="textSecondary" align="center">
+        <Link color="inherit" href="https://example.com/">
+          Justin Connors
+        </Link>
+      </Typography>
+    </footer>
   );
 };
 
