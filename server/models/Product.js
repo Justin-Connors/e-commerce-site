26fb2d862bae528f8463b2fea3759c538f0176ab
justin connors <justinconnors@hotmail.ca>
2024-02-22 21:34:29 -0400

    added product and category schema, no typedefs or resolvers

---
+++ server/models/Product.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const productSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+    min: 0.99,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+    min: 1,
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
